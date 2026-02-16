13d19b2b9331746159e861b1848b83a132665651
Mkz-Prog <mkozyrew@gmail.com>
2026-02-16 22:52:38 +0300

    feat: add server

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox!"))
+}
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 
-func main() { fmt.Println("Hello world!") }
+	log.Println("Start on :4040")
+	err := http.ListenAndServe(":4040", mux)
+	log.Fatal(err)
+}
