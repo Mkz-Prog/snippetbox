a84b620d73842917ea7a7b93e82f1c3ca6460231
Mkz-Prog <mkozyrew@gmail.com>
2026-03-13 15:12:34 +0300

    feat: add logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,14 @@ type application struct {
 	templateCache map[string]*template.Template
 }
 
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	_ = godotenv.Load()
 
