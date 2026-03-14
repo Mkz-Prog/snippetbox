b2a867d35c778a8699354d0d2c435204b3f53a2e
Mkz-Prog <mkozyrew@gmail.com>
2026-03-14 23:30:11 +0300

    feat(middleware): add recoverPanic

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+    "github.com/justinas/alice"
+)
 
-// The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
@@ -13,5 +15,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.logRequest(secureHeaders(mux))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
